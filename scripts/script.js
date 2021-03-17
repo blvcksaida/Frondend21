dda088414fce0cd015b499e3e7055f959d4f1b11
blvcksaida <58034590+blvcksaida@users.noreply.github.com>
2021-03-17 14:37:24 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 // JavaScript Document
+// scroll event voor een sticky button op de home pagina 'download nu de app'
+console.log(document.body)
+console.log("hoi");
+
+var sticky = document.querySelector("header:nth-of-type(2) a");
+
+sticky.addEventListener("scroll", stickyButton);
+
+function stickyButton() {
+  if (sticky > 600) {
+    sticky.addClass("stickybutton");
+  } else {
+    sticky.removeClass("stickybutton");
+  }
+}
